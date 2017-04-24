b6189c94c34bae02378b21bc50b7b7e5f7c2aa0b
Philip Holleran <pholleran@github.com>
2017-04-24 13:32:26 -0500

    update tests

--- test/plugins/projects.js
+++ test/plugins/projects.js
@@ -6,7 +6,6 @@ const payload = require('../fixtures/webhook/issue.created.json');
 const createSpy = expect.createSpy;
 
 describe('projects plugin', () => {
-
   let context;
   let github;
 
@@ -15,19 +14,19 @@ describe('projects plugin', () => {
       projects: {
         createProjectCard: createSpy(),
         getProjectColumns: createSpy(),
-        getRepoProjects: createSpy(),
+        getRepoProjects: createSpy()
       }
     };
     context = new Context(github, {payload});
     this.projects = new Projects();
   });
 
-  describe('create cards', () => {
-    it('creates a card', () => {
-      this.projects.createCard(context, {project:'myProject', column:'myColumn'});
-      
-      expect(github.projects.getRepoProjects).toHaveBeenCalled();
+  describe('createCard', () => {
+    it('creates a project card', () => {
+      this.projects.createCard(context, {project:'myProject', column:'New'}).then(() => {
+        expect(github.issues.create).toHaveBeenCalled({
+        });
+      });
     });
   });
-
 });
