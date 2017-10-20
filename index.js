0c94598b401c8bf238b978adcc43e93457adcd0d
Léo Colombaro <git@colombaro.fr>
2017-10-20 22:48:32 +0200

    use standard linter

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const Configuration = require('./lib/configuration');
-const Context = require('./lib/context');
+const Configuration = require('./lib/configuration')
+const Context = require('./lib/context')
 
 module.exports = robot => {
-  robot.on('*', receive);
+  robot.on('*', receive)
 
-  async function receive(event) {
+  async function receive (event) {
     if (event.payload.repository) {
-      const github = await robot.auth(event.payload.installation.id);
-      const context = new Context(github, event);
-      const config = await Configuration.load(context, '.probot.js');
-      return config.execute();
+      const github = await robot.auth(event.payload.installation.id)
+      const context = new Context(github, event)
+      const config = await Configuration.load(context, '.probot.js')
+      return config.execute()
     }
   }
-};
+}
--- test/plugins/projects.js
+++ test/plugins/projects.js
@@ -1,13 +1,13 @@
-const expect = require('expect');
-const Projects = require('../../lib/plugins/projects');
-const Context = require('../../lib/context');
-const payload = require('../fixtures/webhook/issue.created.json');
+const expect = require('expect')
+const Projects = require('../../lib/plugins/projects')
+const Context = require('../../lib/context')
+const payload = require('../fixtures/webhook/issue.created.json')
 
-const createSpy = expect.createSpy;
+const createSpy = expect.createSpy
 
 describe('projects plugin', () => {
-  let context;
-  let github;
+  let context
+  let github
 
   before(() => {
     github = {
@@ -16,19 +16,19 @@ describe('projects plugin', () => {
         getProjectColumns: createSpy(),
         getRepoProjects: createSpy()
       }
-    };
-    context = new Context(github, {payload});
-    this.projects = new Projects();
-  });
+    }
+    context = new Context(github, {payload})
+    this.projects = new Projects()
+  })
 
   describe('createCard', () => {
     it('creates a project card', () => {
-      this.projects.createCard(context, {project:'myProject', column:'New'}).then(() => {
+      this.projects.createCard(context, {project: 'myProject', column: 'New'}).then(() => {
         expect(github.projects.getRepoProjects).toHaveBeenCalledWith({
           owner: 'pholleran',
           repo: 'test'
-        });
-      });
-    });
-  });
-});
+        })
+      })
+    })
+  })
+})
