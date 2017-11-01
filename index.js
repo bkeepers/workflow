2b69c810f5a916bbae180db90bacb36364033ddc
Léo Colombaro <git@colombaro.fr>
2017-11-01 02:30:20 +0100

    catch error when halted and log it as debug data

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const Configuration = require('./lib/configuration')
 module.exports = robot => {
   robot.on('*', async context => {
     const config = await Configuration.load(context, '.probot.js')
-    return config.execute()
+    return config.execute().catch(err => {
+      if (err.message === 'halted') {
+        robot.log.info(err)
+      } else {
+        throw err
+      }
+    })
   })
 }
